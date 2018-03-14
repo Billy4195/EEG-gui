080633421c377ba266e19a70da7d1c3ddde59700
Billy Su <g4691821@gmail.com>
2018-03-14 20:53:05 +0800

    Use DockArea to do multiple plots

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtGui, QtCore
-import ui as EEG_template
+import ui
 import numpy as np
 import time
 
-app = QtGui.QApplication([])
-
-win = QtGui.QMainWindow()
-ui = EEG_template.Ui()
-ui.setupUi(win)
-
+app = ui.EEG_Application()
 cursor = pg.InfiniteLine(pos=0)
-ui.plot.addItem(cursor)
+app.plot.addItem(cursor)
 cursor_index = 0
 data_list = list()
 curves = list()
@@ -20,11 +15,11 @@ for i in range(64):
     r = (i + 60 ) * 5 % 256
     g = (i + 2 ) * 7 % 256
     b = (i + 15 ) * 11 % 256
-    curve = ui.plot.plot(pen=(r,g,b))
+    curve = app.plot.plot(pen=(r,g,b))
     curve.setData(data)
     curves.append(curve)
     data_list.append(data)
-    ui.plot.resize(5,100)
+    app.plot.resize(5,100)
 
 def update():
     global curves, plot, data, win, cursor_index
@@ -35,7 +30,7 @@ def update():
         start = time.time()
         curves[i].setData(data_list[i])
         end = time.time()
-    ui.plot.setTitle("FPS: {:.1f}".format(1/(time.time()-tol_start)))
+    app.plot.setTitle("FPS: {:.1f}".format(1/(time.time()-tol_start)))
     cursor_index = (cursor_index+25)%2500
     cursor.setValue(cursor_index)
 
@@ -45,5 +40,5 @@ timer.timeout.connect(update)
 timer.start()
 
 
-win.show()
+
 app.exec_()
--- ui.py
+++ ui.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
 
 import pyqtgraph as pg
+from pyqtgraph.dockarea import *
 from PyQt5 import QtCore, QtGui, QtWidgets
+import numpy as np
 
-class Ui(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(500, 600)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
+class EEG_Application(QtGui.QApplication):
+    def __init__(self):
+        super().__init__([])
+        self.main_win = QtGui.QMainWindow()
+        self.main_win.show()
+
+        self.setupUi()
+
+    def setupUi(self):
+        self.main_win.setObjectName("MainWindow")
+        self.main_win.resize(1800, 1000)
+        self.centralwidget = QtWidgets.QWidget(self.main_win)
         self.centralwidget.setObjectName("centralwidget")
-        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
-        self.gridLayout.setObjectName("gridLayout")
+        dockarea = DockArea()
+
+        raw_data_dock = Dock("Raw data plot")
         self.dtypeCombo = QtWidgets.QComboBox(self.centralwidget)
         self.dtypeCombo.setObjectName("dtypeCombo")
         self.dtypeCombo.addItem("")
@@ -18,18 +28,36 @@ class Ui(object):
         self.dtypeCombo.addItem("")
         self.plot = pg.PlotWidget()
         self.plot.setMouseEnabled(x= False, y= True)
-        self.gridLayout.addWidget(self.plot, 1, 0, 2, 2)
-        self.gridLayout.addWidget(self.dtypeCombo, 0, 0, 1, 1)
-        
-        MainWindow.setCentralWidget(self.centralwidget)
+        raw_data_dock.addWidget(self.plot, 1, 0, 2, 2)
+        raw_data_dock.addWidget(self.dtypeCombo, 0, 0, 1, 1)
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        dockarea.addDock(raw_data_dock)
+        left_bottom_dock = self.create_dummy_dock('Left bottom')
+        dockarea.addDock(left_bottom_dock,'bottom',raw_data_dock)
+        mid_top_dock = self.create_dummy_dock('Mid top')
+        dockarea.addDock(mid_top_dock,'right',raw_data_dock)
+        mid_bottom_dock = self.create_dummy_dock("Mid bottom")
+        dockarea.addDock(mid_bottom_dock,'right',left_bottom_dock)
+        right_top_dock = self.create_dummy_dock("Right top")
+        dockarea.addDock(right_top_dock,'right',mid_top_dock)
+        right_bottom_dock = self.create_dummy_dock("Right bottom")
+        dockarea.addDock(right_bottom_dock,'right',mid_bottom_dock)
 
-    def retranslateUi(self, MainWindow):
+        self.main_win.setCentralWidget(dockarea)
+
+        self.retranslateUi()
+        QtCore.QMetaObject.connectSlotsByName(self.main_win)
+
+    def retranslateUi(self):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "EEG"))
+        self.main_win.setWindowTitle(_translate("MainWindow", "EEG"))
         self.dtypeCombo.setItemText(0, _translate("MainWindow", "125"))
         self.dtypeCombo.setItemText(1, _translate("MainWindow", "500"))
         self.dtypeCombo.setItemText(2, _translate("MainWindow", "1000"))
 
+    def create_dummy_dock(self,title="Dummy"):
+        dummy_dock = Dock(title)
+        w3 = pg.PlotWidget(title="Plot inside dock")
+        w3.plot(np.random.normal(size=100))
+        dummy_dock.addWidget(w3)
+        return dummy_dock
