8fbfc9774ce06bf05d3f2e18c0cfaba27ed18c41
kirintw <cy91244@gmail.com>
2018-04-10 23:52:38 +0800

    UI for scale adjustment worked, but refactor and some changes may be required

---
+++ ScaleUI.py
@@ -0,0 +1,32 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QLabel, QSlider, QWidget
+from PyQt5 import QtGui
+import pyqtgraph as pg
+import numpy as np
+import sys
+
+class Slider(QWidget):
+    def __init__(self, current, minimum, maximum, parent=None):
+        super(Slider, self).__init__(parent=parent)
+        self.layout = QtGui.QGridLayout(self)
+        self.label = QLabel(self)
+        self.slider = QSlider(self)
+        self.slider.setOrientation(Qt.Horizontal)
+        self.slider.setRange(minimum, maximum)
+        self.slider.setValue(current)
+
+        self.layout.addWidget(self.label,0,1)
+        self.layout.addWidget(self.slider,0,0)
+        self.resize(self.sizeHint())
+
+        self.slider.valueChanged.connect(self.set_label_value)
+        self.x = None
+        self.set_label_value(self.slider.value())
+
+    def set_label_value(self, value):
+        self.label.setText(str(value))
+    
+    def get_value(self):
+        return self.slider.value()
+
+  
